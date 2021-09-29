b8ecf3e84ccc94e44293937fed76666dca4d4808
imjord <dean.jay.goat@gmail.com>
2021-09-29 14:58:43 -0600

    fixxed footer and about and deployed to heroku

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from '../Nav';
 import picture from '../About/me.jpeg'
-
+import './styles.css'
 
 
 function About(){
@@ -11,7 +11,7 @@ function About(){
         <div className="text-center text-lg-start bg-light text-muted">
              <Nav></Nav>
             <h1 id="contact"  className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"> About </h1>
-            <img src={picture} />
+            <img id="me" src={picture} />
             <p> Hello and welcome to my portfolio! My name is Jordan Im an aspiring full stack web developer with experience in Javascript and MERN. My hobbies are gaming, coding and having hot coco! Thanks for reading! </p>
         </div>
 
